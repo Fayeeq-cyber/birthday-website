ce01f9cac0cb0b1bacb74f9426d82c7aba84045c
Fayeeq-cyber <ahonaffayeeq@gmail.com>
2026-09-12 22:53:43 +0600

    Add surprise route to Flask application

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/surprise")
+def surprise():
+    return render_template("surprise.html")
